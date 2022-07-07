882c66c6a48ecb1b9c80ba79a446b6959843575e
TulikaShandilya <tulikashandilya2422@gmail.com>
2022-07-07 15:51:28 +0530

    Edit Product

--- backend/routers/productRouter.js
+++ backend/routers/productRouter.js
@@ -43,7 +43,7 @@ productRouter.post(
   isAdmin,
   expressAsyncHandler(async (req, res) => {
     const product = new Product({
-      name: 'samle name ' + Date.now(),
+      name: 'sample name ' + Date.now(),
       image: '/images/p1.jpg',
       image1: '/images/p1h1.jpg',
       image2: '/images/p1h2.jpg',
@@ -57,6 +57,29 @@ productRouter.post(
     });
     const createdProduct = await product.save();
     res.send({ message: 'Product Created', product: createdProduct });
+  })
+);
+
+productRouter.put(
+  '/:id',
+  isAuth,
+  isAdmin,
+  expressAsyncHandler(async (req, res) => {
+    const productId = req.params.id;
+    const product = await Product.findById(productId);
+    if (product) {
+      product.name = req.body.name;
+      product.price = req.body.price;
+      product.image = req.body.image;
+      product.category = req.body.category;
+      product.brand = req.body.brand;
+      product.countInStock = req.body.countInStock;
+      product.description = req.body.description;
+      const updatedProduct = await product.save();
+      res.send({ message: 'Product Updated', product: updatedProduct });
+    } else {
+      res.status(404).send({ message: 'Product Not Found' });
+    }
   })
 );
 
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -11,6 +11,7 @@ import OrderHistoryScreen from './screens/OrderHistoryScreen';
 import OrderScreen from './screens/OrderScreen';
 import PaymentMethodScreen from './screens/PaymentMethodScreen';
 import PlaceOrderScreen from './screens/PlaceOrderScreen';
+import ProductEditScreen from './screens/ProductEditScreen';
 import ProductListScreen from './screens/ProductListScreen';
 import ProductScreen from './screens/ProductScreen';
 import ProfileScreen from './screens/ProfileScreen';
@@ -97,6 +98,11 @@ function App() {
               <Route
                 path="/product/:id"
                 element={<ProductScreen />}
+                exact
+              ></Route>
+              <Route
+                path="/product/:id/edit"
+                element={<ProductEditScreen />}
                 exact
               ></Route>
               <Route path="/cart/:id" element={<CartScreen />}></Route>
